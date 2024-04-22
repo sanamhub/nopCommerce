37f2faf68204f60904e5bacd72906a2ff6ef3585
Alexey Anokhin <exile.developer@gmail.com>
2024-04-22 14:56:41 +0300

    7127 Added language detection for neutral cultures by Accept-Language header

---
+++ src/Presentation/Nop.Web.Framework/Globalization/AcceptLanguageHeaderRequestCultureProvider.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Nop.Core.Infrastructure;
+using Nop.Services.Localization;
+
+namespace Nop.Web.Framework.Globalization;
+public class NopAcceptLanguageHeaderRequestCultureProvider : RequestCultureProvider
+{
+    public override Task<ProviderCultureResult> DetermineProviderCultureResult(HttpContext httpContext)
+    {
+        ArgumentNullException.ThrowIfNull(httpContext);
+
+        var acceptLanguageHeader = httpContext.Request.GetTypedHeaders().AcceptLanguage;
+
+        if (acceptLanguageHeader == null || acceptLanguageHeader.Count == 0)
+            return NullProviderCultureResult;
+
+        var languageHeader = acceptLanguageHeader.FirstOrDefault();
+
+        var languageService = EngineContext.Current.Resolve<ILanguageService>();
+        var language = languageService
+            .GetAllLanguages()
+            .FirstOrDefault(urlLanguage => 
+                new CultureInfo(urlLanguage.LanguageCulture).TwoLetterISOLanguageName.Equals(languageHeader.ToString(), StringComparison.InvariantCultureIgnoreCase));
+
+        if (language == null)
+            return NullProviderCultureResult;
+
+        return Task.FromResult(new ProviderCultureResult(language.LanguageCulture));
+
+    }
+}
